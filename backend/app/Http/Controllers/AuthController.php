f9e191c86ee627f483dae010d4d289fc12b9e23b
Paula Joyce Ucol <peaksenut@gmail.com>
2025-02-25 22:15:00 +0800

    feat: set up login route

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -26,7 +27,26 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
-        return 'login';
+        $fields = $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'Incorrect credentials.'
+            ];
+        }
+
+        $token = $user->createToken($request->name . "-> login");
+
+        return [
+            'message' => 'A user has been logged in.',
+            'loggedin_user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function logout(Request $request) {
