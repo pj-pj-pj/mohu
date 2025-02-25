e096f17e8341179d6879ed74daaaea68740daccb
Paula Joyce Ucol <peaksenut@gmail.com>
2025-02-25 21:59:38 +0800

    feat: set up register route and implement user token creation

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
-        return 'register';
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'message' => 'A user has been registered!',
+            'registered_user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function login(Request $request) {
