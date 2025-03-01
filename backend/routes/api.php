0d89ecb812faf4717ab63cd6dfd2668ed2bd0b1c
Paula Joyce Ucol <peaksenut@gmail.com>
2025-03-01 21:55:59 +0800

    add: api resource of quest tracker

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HunterController;
+use App\Http\Controllers\QuestTrackerController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -12,6 +13,7 @@ use App\Http\Controllers\HunterController;
 
 Route::apiResource('quests', QuestController::class);
 Route::apiResource('hunters', HunterController::class)->except(['update']);
+Route::apiResource('quest-tracker', QuestTrackerController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
